11c62904b09f0283d3483c46e835545c79087e7d
Scott Brabazon <>
2024-05-31 16:07:46 +0100

    Google analytics added into foorer

--- components/Footer.js
+++ components/Footer.js
@@ -1,19 +1,26 @@
-import Image from 'next/image';
+import { useEffect } from 'react';
 
 const Footer = () => {
+   useEffect(() => {
+      const script = document.createElement('script');
+      script.src = 'https://www.google-analytics.com/analytics.js';
+      script.async = true;
+
+      script.onload = () => {
+         window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};
+         ga.l=+new Date;
+         ga('create', 'UA-85329524-1', 'auto');
+         ga('send', 'pageview');
+      };
+
+      document.body.appendChild(script);
+   }, []);
 
    let year = new Date().getFullYear();
 
    return (
       <footer>
-            <a href="https://github.com/scottbrabazon" target="blank">
-               <Image src="/img/github.svg" alt="Github" width="22" height="22" />
-            </a>
-            &nbsp;&nbsp;
-            <a href="https://uk.linkedin.com/in/scottbrabazon" target="blank">
-               <Image src="/img/linkedin.svg" alt="LinkedIn" width="22" height="22" />
-            </a>
-            <p>&nbsp;&nbsp;&copy; <span>{year}</span> Scott Brabazon</p>
+         <p>&nbsp;&nbsp;&copy; <span>{year}</span> Scott Brabazon</p>
       </footer>
    );
 }
